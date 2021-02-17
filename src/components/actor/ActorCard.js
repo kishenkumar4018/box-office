bc09abc1e0a235332e3557eae65a335a272abc49
kishenkumar4018 <kishenkumar220@gmail.com>
2021-02-17 17:59:57 +0530

    Installed Style Components

--- src/components/actor/ActorCard.js
+++ src/components/actor/ActorCard.js
@@ -1,9 +1,10 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import { StyledActorCard } from './ActorCard.styled';
 
 const ActorCard = ({ image, name, gender, country, birthday, deathday }) => {
   return (
-    <div>
+    <StyledActorCard  >
       <div>
         <img src={image} alt="actor" />
       </div>
@@ -12,8 +13,9 @@ const ActorCard = ({ image, name, gender, country, birthday, deathday }) => {
       </h1>
       <p>{country ? `Comes from ${country}` : 'No country known'}</p>
       {birthday ? <p>Born {birthday}</p> : null}
-      <p>{deathday ? `Died ${deathday}` : 'Alive'}</p>
-    </div>
+      <p className="deathday">{deathday ? `Died ${deathday}` : 'Alive'}</p>
+    </StyledActorCard>
   );
 };
 export default ActorCard;
+   
