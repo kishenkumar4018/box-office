ef451c4536bb1c11a80e89be5db6c937970cde49
kishenkumar4018 <kishenkumar220@gmail.com>
2021-02-19 17:17:08 +0530

    added loading message and error message in show

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 // eslint-disable-next-line no-unused-vars
 
 import Home from "./pages/Home";
+import Show from "./pages/Show";
 import Starred from "./pages/Starred";
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
     </Route>
     <Route exact path="/Starred" >
         <Starred/>
+    </Route>
+    <Route exact path="/Show/:id">
+      <Show/>
     </Route>
     <Route>
       <div><h1 ><center>Page Not Found</center></h1></div>
---
+++ src/pages/Show.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+import React, {useEffect , useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {apiGet} from "../misc/config";
+
+const Show = () => {
+
+    const {id}= useParams();
+
+    const [show,setShow]= useState(null);
+    const [isLoading, setIsLoading]= useState(true);
+    const [error, setError]= useState(null);
+
+    useEffect(()=> {
+        let isMounted = true;
+        apiGet('/shows/$id}?embed[]=seasons&embed[]=cast').then(results =>{
+             setShow(results);
+             setIsLoading(false);   
+
+        })      
+        
+        .catch(err =>{
+            if (isMounted){
+            setError(err.message);
+            setIsLoading(false);
+        }
+        });
+        return () => {
+            isMounted =false;
+
+        }
+    },[id])
+    console.log('show',show);
+    
+        if(isLoading){
+            return <div>
+                Data is Being Loaded
+            </div>
+        }
+
+        if(error){
+            return<div>
+                Error Occured: { error }
+            </div>
+        }
+
+    return (
+        <div>
+           this is show 
+        </div>
+    )
+};
+
+export default Show;
